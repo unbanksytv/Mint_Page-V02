6f66037e38d20b5452e03ced35cc351611f34840
UnbanksyTV <45092543+unbanksytv@users.noreply.github.com>
2022-07-19 15:36:41 +0200

    constants contracts & web3 css

---
+++ common/constants/contracts.js
@@ -0,0 +1,39 @@
+const contractAddresses = [
+    {
+      name: "Marketplace",
+      description: "Photo Labs Art Gallery Auction",
+      address: "0x3Fe6cfeb8530B3283c4d3E276BCC418FFdc0b748",
+      link: `/marketplace`,
+      icon: `/icons/github.svg`,
+    },
+    {
+      name: "NFT Drop",
+      description: "Claimable drop of one-of-one NFTs",
+      address: "",
+      link: `/nfts/alllistings`,
+      icon: `/icons/github.svg`,
+    },
+    {
+      name: "NFT Collection",
+      description: "A collection of one-of-one NFTs",
+      address: "",
+      link: `/nfts/alllistings`,
+      icon: `/icons/github.svg`,
+    },
+    {
+      name: "Edition Drop",
+      description: "Claimable drop of N-of-one NFTs",
+      address: "0xb1ED41698a311f08880211f6c61655fDCA9C14ed",
+      link: `/nfts/alllistings`,
+      icon: `/icons/github.svg`,
+    },
+    {
+      name: "Edition",
+      description: "A collection of N-of-one NFTs",
+      address: "",
+      link: `/nfts/alllistings`,
+      icon: `/icons/github.svg`,
+    },
+  ];
+  
+  export default contractAddresses;
